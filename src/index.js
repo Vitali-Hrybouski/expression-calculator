d3f2023c26529ec0fb68a8a53b579c02f3177c24
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-21 19:36:00 +0300

    feat: add solution task expression-calculator

--- src/index.js
+++ src/index.js
@@ -3,8 +3,32 @@ function eval() {
     return;
 }
 
+
+function bracketsPair(arr) {
+    let count = 0;
+    arr.forEach(element => {
+        if (element === "(") count++;
+        if (element === ")") count--;
+    });
+    if (count !== 0) throw new Error("ExpressionError: Brackets must be paired");
+}
+function divisionZero(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if ((arr[i] === "/") && (arr[i + 2] === "0")) throw new Error("TypeError: Division by zero.");
+    }
+}
+
 function expressionCalculator(expr) {
-    // write your solution here
+    const arrExpr = expr.split("");
+    bracketsPair(arrExpr);
+    divisionZero(arrExpr);
+
+    let result;
+    const noSpaceString = expr.replace(/\s/g, "");
+    const getResult = new Function("return " + noSpaceString);
+    result = getResult();
+
+    return result;
 }
 
 module.exports = {
